54bdb27edb5278411bca8034475c52c38ed066a2
Enrique Jimenez <enriquejimenez468@gmail.com>
2021-12-04 16:11:54 -0500

    changes to abstract class Game and Place as well ass TriviaGame

--- src/edu/iu/c212/places/games/TriviaGame.java
+++ src/edu/iu/c212/places/games/TriviaGame.java
@@ -6,7 +6,8 @@ import edu.iu.c212.utils.http.HttpUtils;
 public class TriviaGame extends Game {
 
 
-
-
-
+    @Override
+    void onEnter(User user) {
+        HttpUtils.getTriviaQuestions();
+    }
 }
