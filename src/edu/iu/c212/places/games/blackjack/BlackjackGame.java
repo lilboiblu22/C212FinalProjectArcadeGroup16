94323242760c5dc9d882fb0db488dcb30b339154
Joseph <jowamajo@iu.edu>
2021-12-04 21:00:07 -0500

    changing stuff

--- src/edu/iu/c212/places/games/blackjack/BlackjackGame.java
+++ src/edu/iu/c212/places/games/blackjack/BlackjackGame.java
@@ -24,7 +24,7 @@ public class BlackjackGame extends Game {
     private static JLabel totalsLabel;
     private static JButton hit;
     private static JButton stay;
-    private static int[] nums = new int[]{1,2,3,4,5,6,7,8,9,10,10,10,10};
+    private static int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
     private static Random rand = new Random();
     private static boolean isAOne;
 
@@ -32,40 +32,28 @@ public class BlackjackGame extends Game {
     public BlackjackGame(Arcade arc) {
 
     }
+
+    @Override
+    public void play() {
+
+    }
+
     public void onEnter(User user) {
+        BlackjackPlayer player = new BlackjackPlayer();
+        BlackjackDealer dealer = new BlackjackDealer();
+
+        JFrame frame = new JFrame();
+        frame = new JFrame();
+        frame.setTitle("MouseListenerDemo");
+        frame.setSize(420,420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel mainPanel = new JPanel();
+        JPanel statusPanel = new JPanel();
+
+
 
     }
-    public static class hitButtonListener implements ActionListener {
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            int addNum = nums[rand.nextInt(nums.length)];
-            // we want to add numbers to both first and second, regardless, but then add differently and show as long as second is
-            if ((handTotals[0] + addNum) > 21) {
-                totalsLabel.setText("BUST");
-                hit.setEnabled(false);
-                stay.setEnabled(false);
-                System.out.println("bust");
-                totalsLabel.setText("BUST");
-            } else {
-
-                if (addNum == 1) {
-                    isAOne = true;
-                    handTotals[0] += 1;
-                    handTotals[1] += 11;
-                } else {
-                    handTotals[0] += addNum;
-                    handTotals[1] += addNum;
-                }
-                if (handTotals[0] != handTotals[1] && handTotals[1] < 21) {
-                    totalsLabel.setText("Your first  hand total: " + handTotals[0] + "and your second hand total: " + handTotals[1]);
-                } else {
-                    totalsLabel.setText("Your hand total: " + handTotals[0]);
-                }
-                System.out.println(isAOne);
-            }
-        }
-    }
     public static class stayButtonListener implements ActionListener {
 
         @Override
@@ -76,50 +64,4 @@ public class BlackjackGame extends Game {
 
         }
     }
-
-    public static void main(String[] args) {
-
-        JFrame frame = new JFrame();
-        frame = new JFrame();
-        frame.setTitle("MouseListenerDemo");
-        frame.setSize(420,420);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel mainPanel = new JPanel();
-        JPanel statusPanel = new JPanel();
-        int firstNum = nums[rand.nextInt(nums.length)];
-        int secondNum = nums[rand.nextInt(nums.length)];
-        if (firstNum == 1) {
-            handTotals[0] = 1 + secondNum;
-            handTotals[1] = 11 + secondNum;
-        }
-        else if (secondNum == 1) {
-            handTotals[0] = 1 + firstNum;
-            handTotals[1] = 11 + firstNum;
-        }
-        int sum = firstNum + secondNum;
-        System.out.println("sum " + sum);
-        handTotals[0] = sum;
-        handTotals[1] = sum;
-        totalsLabel = new JLabel("Your hand total: " + sum);
-        statusPanel.add(totalsLabel);
-        JPanel buttonsPanel = new JPanel();
-        JLabel dealerLabel = new JLabel("Dealer's hand: " + nums[rand.nextInt(nums.length)] + ", ???");
-
-        hit = new JButton("Hit!");
-        stay = new JButton("stay");
-        buttonsPanel.add(hit);
-        buttonsPanel.add(stay);
-        statusPanel.add(dealerLabel);
-        mainPanel.add(statusPanel);
-        mainPanel.add(buttonsPanel);
-        hit.addActionListener(new hitButtonListener());
-        stay.addActionListener(new stayButtonListener());
-
-
-        frame.add(mainPanel);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    }
-
 }
--- src/edu/iu/c212/places/games/blackjack/BlackjackParticipant.java
+++ src/edu/iu/c212/places/games/blackjack/BlackjackParticipant.java
@@ -1,12 +1,14 @@
 package edu.iu.c212.places.games.blackjack;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public abstract class BlackjackParticipant {
-    protected static ArrayList<String> cards;
+    private static ArrayList<Integer> cards = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,10,10,10));
     protected int [] handTotals;
     public void hit() {
 
-    };
+
+    }
     public abstract int getBestTotal();
 }
